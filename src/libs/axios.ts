1286dd13380ec8731764466f5eb9927f3ff513d3
tapendrarsc <tapendra@rscgroupdholera.in>
2025-09-04 10:58:43 +0530

    changes

--- src/libs/axios.ts
+++ src/libs/axios.ts
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:8000/",
+    baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000/",
     withCredentials: true,
 });
 
